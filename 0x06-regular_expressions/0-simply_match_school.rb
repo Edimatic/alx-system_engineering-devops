0181e003e34d35c29acac14887336d77a46104f5
Edimatic <ediomiudofia@gmail.com>
2024-02-27 05:03:35 -0800

    Simply matching School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,23 +1,3 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-  exit 1
-end
-
-# Get the input string from the command line argument
-input_string = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /School/
-
-# Use the scan method to find all occurrences of the pattern in the input string
-matches = input_string.scan(pattern)
-
-# Output the result
-if matches.any?
-  puts "#{matches.join}$"
-else
-  puts "$"
-end
+# A regular expression that is simply matching School
+puts ARGV[0].scan(/School/).join
