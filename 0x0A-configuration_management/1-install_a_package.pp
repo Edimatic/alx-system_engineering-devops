b012727fd04ae75254d6ad15f8f8ba600bea8085
Edimatic <ediomiudofia@gmail.com>
2024-04-20 10:00:22 -0700

    Install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,10 @@
 # Using Puppet, install flask from pip3
 
 package { 'Werkzeug':
-package { 'flask':
-  ensure   => '2.1.0',
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
+package { 'flask':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
